d694363711d876045360142349fc791c9bc080a1
heeejoo0518 <heeejoo0518@gmail.com>
2022-05-19 16:19:46 +0900

    @Mapper

--- happyhouse_final_be/src/main/java/com/ssafy/happyhouse/mapper/AreaMapper.java
+++ happyhouse_final_be/src/main/java/com/ssafy/happyhouse/mapper/AreaMapper.java
@@ -2,8 +2,11 @@ package com.ssafy.happyhouse.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.ssafy.happyhouse.dto.AreaDto;
 
+@Mapper
 public interface AreaMapper {
 	int addArea(String userId, String dongCode);
 	int deleteArea(String areaNo);
