e6980eb6c927ed99fbd44b4be29f8c2a43ee6288
Kim-Ji-soo <kimjs1995@naver.com>
2022-05-21 22:15:40 +0900

    HouseMap 마커찍기

--- happyhouse_final_vue/src/components/house/HouseMap.vue
+++ happyhouse_final_vue/src/components/house/HouseMap.vue
@@ -7,12 +7,21 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
+const houseStore = "houseStore";
+
 export default {
+  name: "HouseMap",
   data() {
     return {
       map: null,
+      markers: [],
     };
   },
+  computed: {
+    ...mapState(houseStore, ["dongs", "house", "houses"]),
+  },
+
   methods: {
     initMap() {
       const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
@@ -22,6 +31,43 @@ export default {
         level: 5, //지도의 레벨(확대, 축소 정도)
       };
       this.map = new kakao.maps.Map(container, options);
+    },
+
+    displayMarker(markers) {
+      var positions = [];
+      markers.forEach((marker) => {
+        positions.push({
+          title: marker.apartmentName,
+          latlng: new kakao.maps.LatLng(marker.lat, marker.lng),
+        });
+      });
+
+      // 마커 이미지의 이미지 주소입니다
+      var imageSrc =
+        "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+      for (var i = 0; i < positions.length; i++) {
+        // 마커 이미지의 이미지 크기 입니다
+        var imageSize = new kakao.maps.Size(24, 35);
+
+        // 마커 이미지를 생성합니다
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+          map: this.map, // 마커를 표시할 지도
+          position: positions[i].latlng, // 마커를 표시할 위치
+          title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+          image: markerImage, // 마커 이미지
+        });
+        marker.setMap(this.map);
+      }
+    },
+  },
+  watch: {
+    houses: function () {
+      this.markers = this.houses;
+      this.displayMarker(this.markers);
     },
   },
   mounted() {
