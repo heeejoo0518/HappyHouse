718cacc3c2aa3fe320f69a52338eb7dcb4d92dc8
heeejoo0518 <heeejoo0518@gmail.com>
2022-05-24 11:05:43 +0900

    Update index.js

--- happyhouse_final_vue/src/router/index.js
+++ happyhouse_final_vue/src/router/index.js
@@ -17,7 +17,7 @@ const onlyAuthUser = async (to, from, next) => {
   }
   if (checkUserInfo === null) {
     alert("로그인이 필요한 페이지입니다..");
-    next({ name: "signIn" });
+    next({ name: "signin" });
     // router.push({ name: "signIn" });
   } else {
     // console.log("로그인 했다.");
